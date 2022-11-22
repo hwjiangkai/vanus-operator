936da4c7de71d10a0737fd00da4e69b653b14473
jyjiangkai <jyjiangkai@163.com>
2022-11-22 11:12:24 +0800

    bakeup code

--- api/v1/store_types.go
+++ api/v1/store_types.go
@@ -1,18 +1,16 @@
-/*
-Copyright 2022.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2022 Linkall Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package v1
 
@@ -34,7 +32,7 @@ type StoreSpec struct {
 	// in the event of a fragmenting network partition.
 	// +optional
 	// +kubebuilder:validation:Minimum:=0
-	// +kubebuilder:default:=1
+	// +kubebuilder:default:=3
 	Replicas int32 `json:"replicas,omitempty"`
 	// Image is the name of the controller docker image to use for the Pods.
 	// Must be provided together with ImagePullSecrets in order to use an image in a private registry.
